1e7ca798304b03b18e02059aa0bd1822dd6674f5
Luc Thuy <nguyenducminh01122002@gmail.com>
2023-03-30 10:27:22 +0700

    initialize project

---
+++ src/main/java/com/tododiary/tododiarybe/TododiaryBeApplication.java
@@ -0,0 +1,13 @@
+package com.tododiary.tododiarybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TododiaryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TododiaryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tododiary/tododiarybe/TododiaryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tododiary.tododiarybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TododiaryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
