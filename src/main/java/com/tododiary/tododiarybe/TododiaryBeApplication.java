df6aa2c350d3e9426c6b0ab5706ef04611412d74
Luc Thuy <nguyenducminh01122002@gmail.com>
2023-04-25 18:36:10 +0700

    service and avatar handle/ on progress

--- src/main/java/com/tododiary/tododiarybe/TododiaryBeApplication.java
+++ src/main/java/com/tododiary/tododiarybe/TododiaryBeApplication.java
@@ -1,13 +1,20 @@
 package com.tododiary.tododiarybe;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TododiaryBeApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TododiaryBeApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
